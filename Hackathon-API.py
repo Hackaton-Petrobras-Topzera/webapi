26bd9844c46cf6acdbe7cd3ff3ba35d20dec94d2
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-10-01 06:42:18 -0300

    CORS fix

--- Hackathon-API.py
+++ Hackathon-API.py
@@ -1,6 +1,7 @@
 import json
 
 from flask import Flask, jsonify
+from flask.ext.cors import CORS, cross_origin
 
 from service.database import Dao
 from service.service import GasStationService, ProductService, OrderService
@@ -22,42 +23,50 @@ def to_json(data):
 
 
 @app.route('/')
+@cross_origin()
 def hello_world():
     return 'Hello World!'
 
 
 @app.route('/gasstation')
+@cross_origin()
 def gas_station():
     all_gas = GasStationService().select_all_gas()
     return to_json(all_gas)
 
 
 @app.route('/products/all')
+@cross_origin()
 def get_all_products():
     products = ProductService().select_all()
     return to_json(products)
 
 
 @app.route('/products/<id>')
+@cross_origin()
 def get_product_by_id(id):
     product = ProductService().find_by_id(int(id))
     return to_json(product)
 
 
 @app.route('/products/<id>/arrangements')
+@cross_origin()
 def get_only_arrangements(id):
     product = ProductService().search_only_arrangement(int(id))
     return to_json(product)
 
 
 @app.route('/connection/status')
+@cross_origin()
 def connection_status():
     dao = Dao()
     dao.connect()
     dao.close_connection()
     return jsonify(connection=True), 200
 
+
 @app.route('/orders/opened')
+@cross_origin()
 def get_all_opened_orders():
     orders = OrderService().select_opened_orders()
     return to_json(orders)
