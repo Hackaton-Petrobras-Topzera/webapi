d26705ab522e1dcc2b7ec847c90523177a11f723
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-10-01 05:54:57 -0300

    removing lat and long as string

--- service/database.py
+++ service/database.py
@@ -31,7 +31,8 @@ class LocationDao(Dao):
 
         all_gas_station_from_rj = collection.find({
             "state": "RJ",
-            "latitude": {"$ne": "NULL"}
+            "latitude": {"$ne": "NULL"},
+            "latitude": {"$type": "int"}
         }, {
             "latitude": 1,
             "longitude": 1,
